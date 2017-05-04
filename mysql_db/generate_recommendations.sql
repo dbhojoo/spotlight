768734375942db30377d84c5c64cd48fe94d438c
dbhojoo <demisbhojoo@gmail.com>
2017-05-04 22:00:11 +0100

    recommendations
    recommendation might need to be done on the fly and we will need track
    these

---
+++ mysql_db/generate_recommendations.sql
@@ -0,0 +1,15 @@
+--all CORE tables are normalised meaning that there should not be any duplicates present
+--this is to create the recommendations table, a recommendation is any event ranked and shown to the user
+
+CREATE TABLE activities (
+	id int NOT NULL unique
+	, user_id int
+	, event_id int
+	, rank int
+	, accepted binary(1)
+	, created_at datetime
+	, updated_at datetime
+	, PRIMARY KEY (id)
+	)
+	
+	;
